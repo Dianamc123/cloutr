4afe85dc12ebb5f3729f4eaa7339233858250975
Andres Rodriguez <andres_rs@live.com.ar>
2018-06-30 18:45:29 -0500

    [Calendar] Finis

--- dashboard-front/js/main.js
+++ dashboard-front/js/main.js
@@ -7,4 +7,24 @@ $(document).ready(function(){
 
 
     $('#pet').popover('show')
+
+    // calendar 
+    $("#calendar").fullCalendar({
+        height: 'parent',
+        contentHeight: "auto",
+         header: {
+            left: 'prev',
+            center: 'title',
+            right: 'next'
+        },
+        showNonCurrentDates :false,
+        viewRender :function(view, element){
+            var mes = view.start.format("MMMM");
+            var year = view.start.format("Y");
+            var $title = $("#calendar .fc-toolbar .fc-center");
+            
+            $title.html("<h2><span clas='mes'>"+mes+"</span><br> <span class='year'>"+year+"</span>" );
+            console.log(view, element)
+        }
+    });
 });
