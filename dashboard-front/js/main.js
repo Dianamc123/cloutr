ac169839593efab10554a089dd0efa2d3dbfe5ff
Andres Rodriguez <andres_rs@live.com.ar>
2018-07-02 20:28:15 -0500

    [DAS] Responsive

--- dashboard-front/js/main.js
+++ dashboard-front/js/main.js
@@ -1,13 +1,42 @@
 $(document).ready(function(){
 
+    // pet dialo
     $("#pet").popover({
         html : true,
         content : "<b>Need some help?</b>"
     });
 
+    //  collapse oters list
+    $("[data-toggle=collapse]").click(function(){
 
-    $('#pet').popover('show')
+        var _tis = $(this);
+        var $collpase = $("[data-toggle=collapse]").not(_tis);
 
+        $collpase.each(function(){
+            var taret = $(this).data("target");
+            $(taret).collapse("hide");
+        })
+    });
+
+
+    //  pet tole navbar
+    $('#pet').click(function(){ 
+      var _tis = $(this);
+      var bottom =  parseInt( _tis.css("bottom") );
+    
+      if(bottom < 0 )
+      {
+         _tis.css("bottom",0);
+         _tis.popover("show");
+      }else{
+        _tis.popover("hide");
+        _tis.css("bottom","-2rem");
+      }
+    
+    });
+
+    // 
+    $(".date").datepicker();
     // calendar 
     $("#calendar").fullCalendar({
         height: 'parent',
@@ -22,9 +51,8 @@ $(document).ready(function(){
             var mes = view.start.format("MMMM");
             var year = view.start.format("Y");
             var $title = $("#calendar .fc-toolbar .fc-center");
-            
+            /// reasin title calendar
             $title.html("<h2><span clas='mes'>"+mes+"</span><br> <span class='year'>"+year+"</span>" );
-            console.log(view, element)
         }
     });
 });
